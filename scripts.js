7eab1521160318816bd154aebd1714fee8b4ae06
System Administrator <root@anons-MacBook-Pro.local>
2021-07-30 22:42:59 -0400

    Search bar centering, column text fixed

--- scripts.js
+++ scripts.js
@@ -3,6 +3,7 @@ $(document).ready(function () {
 	var DOM = {}
 	DOM.$ethPriceBox = $("#box-price-eth-usd");
 	DOM.$allPricesBox = $("#box-price-all-usd");
+	DOM.$memeData = $("#box-price-meme")
 	DOM.$hamburgerWrapper = $(".hamburger-wrapper");
 	DOM.$hamburger = $(".hamburger");
 	DOM.$hamburgerId = $("#hamburger");
@@ -12,10 +13,16 @@ $(document).ready(function () {
 		try {
 			var ethPrices = await getCryptoPricesByUrl('https://min-api.cryptocompare.com/data/price?fsym=ETH&tsyms=BTC,USD,EUR');
 			var allPrices = await getCryptoPricesByUrl('https://min-api.cryptocompare.com/data/pricemultifull?fsyms=BTC&tsyms=USD,EUR&api_key=7eb7356d9e00768b85fb67c9da901b296df7af690939b4d9f8f5679ee355c54a');
+			var memeData = await getCryptoPricesByUrl('https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&category=meme-token&order=market_cap_desc&per_page=100&page=1&sparkline=false&price_change_percentage=7d');
+			let memeTestObject = {}
+			memeTestObject['1'] = 'value'
+			console.log(memeTestObject[1]);
+			console.log(memeData);
 			console.log(allPrices);
-			console.log(ethPrices);  
+			console.log(ethPrices);
 			DOM.$ethPriceBox.text(ethPrices.USD);
 			DOM.$allPricesBox.text(allPrices.DISPLAY.BTC.USD.CHANGEPCTDAY);
+			DOM.$memeData.text(memeData[1].ath);
 		} catch (err) {
 			console.log(err);
 		}
@@ -33,3 +40,4 @@ $(document).ready(function () {
 		});
 	}
 });
+
